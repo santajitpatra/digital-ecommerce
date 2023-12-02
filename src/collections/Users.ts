28a87ffe7ff34b75590e145ad2cf5bbfeb0a5708
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-02 23:57:07 +0530

    feat : email verify
    feat : email verify not working

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<P>hello please verify</P>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
