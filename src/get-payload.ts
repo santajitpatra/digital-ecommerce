fcb6c04a430f1c14fdcdb7e0069581fa9f840447
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-02-19 23:58:45 +0530

    fix : new fromAddress
    fix : new fromAddress

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -47,8 +47,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress:
-          "business.santajitpatra@santajitpatra.netlify.app",
+        fromAddress: "onboarding@resend.dev",
         fromName: "DigitalEcommerce",
       },
       secret: process.env.PAYLOAD_SECRET,
