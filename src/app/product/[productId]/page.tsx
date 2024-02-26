036e85e89478801757bd08d03df4943cdbb37a21
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-02-27 02:46:49 +0530

    feat: Implement AddToCartButton with Zustand (learning exploration)
    feat: Implement AddToCartButton with Zustand (learning exploration)
    
    This commit introduces the AddToCartButton component utilizing Zustand for state management. It also represents the exploration and learning phase of Zustand functionalities. Further enhancements and customization are expected.

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import AddToCartButton from "@/components/AddToCartButton";
 import ImageSlider from "@/components/ImageSlider";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import ProductReel from "@/components/ProductReel";
@@ -126,6 +127,7 @@ const Page = async ({ params }: PageProps) => {
           <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
             <div>
               <div className="mt-10">
+                <AddToCartButton product={product} />
               </div>
               <div className="mt-6 text-center">
                 <div className="group inline-flex text-sm text-medium">
